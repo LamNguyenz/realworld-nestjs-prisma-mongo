43be4e70c41e6e2dfe26e5beef81510846f5c3d3
Lam Nguyen <lamnguyen11e@gmail.com>
2024-11-09 14:14:13 +0700

    feat(Article): git articles without filter

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,13 +1,62 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { User, Prisma } from '@prisma/client';
 import { PrismaService } from 'prisma/prisma.service';
 import { ArticleForCreateDto, castToArticle } from './dto';
 import { PrismaClientValidationError } from '@prisma/client/runtime/library';
-import { castToProfile } from 'src/profiles/dto';
+import { castToProfile, ProfileDto } from 'src/profiles/dto';
 
 @Injectable()
 export class ArticlesService {
   constructor(private prisma: PrismaService) {}
+
+  async getArticles(
+    user: User,
+    query?: {
+      tag?: string;
+      author?: string;
+      favorited?: string;
+      limit?: number;
+      offset?: number;
+    },
+  ) {
+    const { tag, author, favorited, limit = 10, offset = 0 } = query;
+    const queryArgs: Prisma.ArticleFindManyArgs = {
+      where: {
+        author: {
+          username: author,
+        },
+      },
+    };
+    const [articles, articlesCount] = await Promise.all([
+      this.prisma.article.findMany({
+        ...queryArgs,
+        take: limit,
+        skip: offset,
+        include: {
+          author: true,
+        },
+        orderBy: {
+          updatedAt: 'desc',
+        },
+      }),
+      this.prisma.article.count({
+        where: queryArgs.where,
+      }),
+    ]);
+
+    const articlesDto = articles.map((article) => {
+      const following =
+        article.author?.followersIds.includes(user?.id || '') || false;
+      let authorProfile: ProfileDto;
+      if (!article.author) {
+        authorProfile = null;
+      } else {
+        authorProfile = castToProfile(article.author, following);
+      }
+      return castToArticle(article, user, article.tagList, authorProfile);
+    });
+    return { articles: articlesDto, articlesCount };
+  }
 
   async createArticle(user: User, articleToCreate: ArticleForCreateDto) {
     const slug = articleToCreate.title.split(' ').join('-');
